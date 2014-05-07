c9b64be611349a4cd270249c851f92eb97185fae
acullather <anthonycullather.jr@gmail.com>
2014-05-07 12:08:55 -0500

    master commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+using FizzBuzzClass;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FizzBuzzClass.FizzBuzzClass.FizzBuzz(1, 100);
+            using (FileStream fs = new FileStream(@"c:\test\fizzbuzz.txt", FileMode.OpenOrCreate))
+            {
+                FizzBuzzClass.FizzBuzzClass.FizzBuzz(1, 10000, fs);
+            }
+            Console.ReadLine();
+        }
+
+    }
+
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FizzBuzzTest()
+        {
+            //Arrange
+            int start = 1;
+            int end = 100;
+            bool completed = false;
+
+            //Act
+            completed = FizzBuzzClass.FizzBuzzClass.FizzBuzz(start, end);
+
+            //Assert
+            Assert.IsTrue(completed);
+        }
+
+        [TestMethod]
+        public void FailTest()
+        {
+            int start = 100;
+            int end = 1;
+            bool completed = false;
+
+            completed = FizzBuzzClass.FizzBuzzClass.FizzBuzz(start, end);
+
+            Assert.IsFalse(completed);
+        }
+    }
+}
